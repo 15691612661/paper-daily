8533ad3eeac7eb24d544169eac66ec0e13937b92
Zhang-liang Wang <73271928+15691612661@users.noreply.github.com>
2022-04-27 23:39:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # The repository to add this issue to
-REPO_OWNER = 'DongZhouGu'
-REPO_NAME = 'arxiv-daily'
+REPO_OWNER = '15691612661'
+REPO_NAME = 'paper-daily'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["human object interaction", "visual relation detection", "object detection", "transformer",
-                "scene understanding", "visual reasoning"]
+KEYWORD_LIST = ["time series", "power forecasting", "time decomposition", "transformer",
+                "function fitting", "LSTM","copula","distribution"]
